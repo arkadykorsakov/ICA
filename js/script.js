54a9c4bb7722813935de7614d4b847ef4fd5e951
Arkady Korsakov <arkadiy.korsakov.98@mail.ru>
2023-02-28 00:21:34 +0700

    third commit

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ const body = document.body
 
 if (iconMenu) {
   iconMenu.addEventListener('click', function () {
-    selectOptions.classList.remove('active')
-    arrow.classList.remove('active')
-
     menuBody.classList.toggle('active')
     iconMenu.classList.toggle('active')
     body.classList.toggle('lock')
@@ -16,39 +13,62 @@ if (iconMenu) {
 /* </АДАПТИВНОЕ МЕНЮ> */
 
 // <SELECT>
-const selectLabel = document.querySelector('.select__label')
-const selectOptions = document.querySelector('.select__options')
-const selectTitle = document.querySelector('.select__title')
-const arrow = document.querySelector('.select__arrow')
-const options = document.querySelectorAll('.option')
+const selectes = document.querySelectorAll('.select')
 
-if (selectLabel) {
-  selectLabel.addEventListener('click', function () {
-    menuBody.classList.remove('active')
-    iconMenu.classList.remove('active')
-    body.classList.remove('lock')
+if (selectes.length) {
+  selectes.forEach((select) => {
+    const selectLabel = select.querySelector('.select__label')
+    const selectTitle = selectLabel.querySelector('.select__title')
+    const selectArrow = select.querySelector('.select__arrow')
+    const selectOptions = select.querySelector('.select__options')
+    const options = selectOptions.querySelectorAll('.select__option')
+    const selectInput = select.querySelector('.select__input')
 
-    selectOptions.classList.toggle('active')
-    arrow.classList.toggle('active')
+    selectLabel.addEventListener('click', function () {
+      if (select.closest('.menu__lang')) {
+        menuBody.classList.remove('active')
+        iconMenu.classList.remove('active')
+        body.classList.remove('lock')
+      }
+
+      selectOptions.classList.toggle('active')
+      selectArrow.classList.toggle('active')
+    })
+
+    if (options.length) {
+      options.forEach((option) =>
+        option.addEventListener('click', function (e) {
+          e.stopPropagation()
+          selectTitle.innerText = this.innerText
+          selectInput.value = this.dataset.value
+          if (select.classList.contains('menu__lang')) {
+            selectInput.click()
+          }
+          selectOptions.classList.remove('active')
+          selectArrow.classList.remove('active')
+        })
+      )
+    }
+
+    document.addEventListener('click', function (e) {
+      if (
+        !e.target.closest('.select') ||
+        !(e.target.closest('.select') === select)
+      ) {
+        selectOptions.classList.remove('active')
+        selectArrow.classList.remove('active')
+      }
+    })
+
+    document.addEventListener('keydown', function (e) {
+      if (e.key === 'Tab' || e.key === 'Escape') {
+        selectOptions.classList.remove('active')
+        selectArrow.classList.remove('active')
+      }
+    })
   })
 }
 
-if (options.length) {
-  options.forEach((option) =>
-    option.addEventListener('click', () => {
-      selectTitle.innerHTML = option.querySelector('label').innerHTML
-      selectOptions.classList.remove('active')
-      arrow.classList.remove('active')
-    })
-  )
-}
-
-window.addEventListener('click', function (event) {
-  if (!event.target.closest('.select')) {
-    selectOptions.classList.remove('active')
-    arrow.classList.remove('active')
-  }
-})
 // </SELECT>
 
 // <FORM>
@@ -59,6 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   async function formSend(e) {
+    console.log(e)
     e.preventDefault()
 
     let error = formValidate(form)
@@ -91,25 +112,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for (let index = 0; index < formReq.length; index++) {
       const input = formReq[index]
-      const errorItem = input.nextElementSibling
+      const formItem = input.closest('.form__item')
+      const formError = formItem.querySelector('.form__error')
+
+      console.log(formError)
 
       formRemoveError(input)
-      errorItem.innerHTML = ''
+      formError.innerHTML = ''
       if (input.value === '') {
         formAddError(input)
-        errorItem.innerHTML = 'Поле должно быть заполено'
+        formError.innerHTML = 'Поле должно быть заполнено'
         error++
       } else {
         if (input.classList.contains('_email')) {
           if (emailTest(input)) {
             formAddError(input)
-            errorItem.innerHTML = 'Некорректный email'
+            formError.innerHTML = 'Некорректный email'
             error++
           }
         } else if (input.classList.contains('_tel')) {
           if (telTest(input)) {
             formAddError(input)
-            errorItem.innerHTML = 'Некорректный номер'
+            formError.innerHTML = 'Некорректный номер'
             error++
           }
         }
@@ -119,11 +143,11 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function formAddError(input) {
-    input.parentElement.classList.add('_error')
+    input.closest('.form__item').classList.add('_error')
   }
 
   function formRemoveError(input) {
-    input.parentElement.classList.remove('_error')
+    input.closest('.form__item').classList.remove('_error')
   }
 
   // Функция проверки email
@@ -139,3 +163,212 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 })
 // </FORM>
+
+/* <SLIDER> */
+const imageSlider = document.querySelector('.image-slider')
+// инициализация
+if (imageSlider) {
+  new Swiper('.image-slider', {
+    // стрелки
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+
+    // включение/отключение перетаскивания на ПК
+    simulateTouch: false,
+
+    // чувствительность свайпа
+    touchRatio: 1,
+
+    // угол срабатывания свайпа/перетаскивания
+    touchAngle: 45,
+
+    // курсор перетаскивания
+    grabCursor: false,
+
+    // преключение при клике на слайд
+    slideToClickedSlide: false,
+
+    // навигация по хэшу
+    hashNavigation: {
+      // отслеживать состояние
+      watchState: true
+    },
+
+    //переключение слайдов клавиатурой
+    keyboard: {
+      // включить/выключить
+      enabled: true,
+      // включить/выключить, когда слайдер в зоне видимости
+      onlyInViewport: false,
+      //управление клавшами pageUp pageDown
+      pageUpDown: true
+    },
+
+    // переключение слайдов мышью
+    mousewheel: {
+      // чувствительность колеса мыши
+      sensitivity: 1,
+
+      //объект, на котором будет срабатывать прокрутка мышью
+      eventsTarget: '.image-slider'
+    },
+
+    // автовысота
+    autoHeight: true,
+
+    //отключение функционала, если слайдов меньше, чем нужно
+    watchOverflow: true,
+
+    //отступы между слайдами
+    spaceBetween: 90,
+
+    //количество пролистываемых слайдов
+    slidesPerGroup: 1,
+
+    //активный слайд по центру
+    //   centeredSlides: true,
+
+    // активный слайд с 0
+    initialSlide: 0,
+
+    //мультирядность
+    slidesPerColumn: 1, //при autoHeight:false
+
+    //бесконечный слайдер
+    loop: false, //скролл не работает, отключить
+
+    //количество дублирующих слайдов
+    loopedSlides: 0,
+
+    //свободный режим
+    freeMode: true,
+
+    breakpoints: {
+      1440: {
+        //отступы между слайдами
+        spaceBetween: 30,
+        slidesPerView: 3
+      },
+      1200: {
+        //отступы между слайдами
+        spaceBetween: 50,
+        slidesPerView: 3
+      },
+      991.98: {
+        //отступы между слайдами
+        spaceBetween: 60,
+        // количество слайдов для показа
+        slidesPerView: 3
+      },
+      767.98: {
+        // количество слайдов для показа
+        slidesPerView: 2
+      }
+    }
+  })
+
+  const lightbox = new FsLightbox()
+  fsLightboxInstances[''].props.onOpen = () => {
+    console.log(fsLightboxInstances)
+  }
+  //   lightbox.props.sources = ['img/gallery/1.jpg']
+  //   lightbox.close()
+  //   lightbox.open(1)
+}
+
+/* </SLIDER> */
+
+// <МУЛЬТИЯЗЫЧНОСТЬ>
+const allLangs = ['ru', 'en']
+let currentLang = localStorage.getItem('lang') ?? checkBrowserLang()
+const menuLang = document.querySelector('.menu__lang')
+const currentPath = window.location.pathname
+let currentText = {}
+
+checkPagePathName()
+changeLang()
+
+if (menuLang) {
+  menuLang.querySelector('.select__title').textContent = currentLang
+
+  const menuLangInput = menuLang.querySelector('.select__input')
+  if (menuLangInput) {
+    menuLangInput.addEventListener('click', function () {
+      console.log('ghnjm,s')
+      menuLang.querySelector('.select__title').textContent = menuLangInput.value
+      currentLang = menuLangInput.value
+      localStorage.setItem('lang', menuLangInput.value)
+      changeLang()
+    })
+  }
+}
+
+function checkPagePathName() {
+  switch (currentPath) {
+    case '/index.html':
+      currentText = {
+        title: {
+          ru: 'Мы делаем то, <br /> чего не могут другие',
+          en: "We do what others can't do"
+        },
+        social: {
+          ru: ` <a href="https://vk.com/domoratskiymaxim" class="social__link vk" target="_blank"></a>`,
+          en: ` <a href="https://www.facebook.com/DomoratskiyMaxim" class="social__link facebook" target="_blank"></a>
+			 <a href="https://www.instagram.com/domoratskiy_maxim/" class="social__link inst" target="_blank"></a>`
+        }
+      }
+      break
+    case '/about.html':
+      currentText = {
+        title: {
+          ru: 'Международное IR-Агентство Инвестиционных Коммуникаций «ICA»',
+          en: 'International IR Agency for Investment Communications  «ICA»'
+        }
+      }
+      break
+    case '/investitions.html':
+      break
+    case '/services.html':
+      break
+    case '/siberian_falconry.html':
+      currentText = {}
+      break
+    default:
+      currentText = {
+        title: {
+          ru: 'Мы делаем то, <br /> чего не могут другие',
+          en: "We do what others can't do"
+        },
+        social: {
+          ru: ` <a href="https://vk.com/domoratskiymaxim" class="social__link vk" target="_blank"></a>`,
+          en: ` <a href="https://www.facebook.com/DomoratskiyMaxim" class="social__link facebook" target="_blank"></a>
+			  <a href="https://www.instagram.com/domoratskiy_maxim/" class="social__link inst" target="_blank"></a>`
+        }
+      }
+      break
+  }
+}
+
+function changeLang() {
+  for (const key in currentText) {
+    const elem = document.querySelector(`[data-lang-${key}]`)
+    if (elem) {
+      elem.innerHTML = currentText[key][currentLang]
+    }
+  }
+}
+
+function checkBrowserLang() {
+  const navLang = navigator.language.slice(0, 2).toLowerCase()
+  const result = allLangs.some((elem) => {
+    return elem === navLang
+  })
+  if (result) {
+    return navLang
+  } else {
+    return 'en'
+  }
+}
+// </МУЛЬТИЯЗЫЧНОСТЬ>
